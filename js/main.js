7b96672d1be2181e032ec995074d12f41b16e2d2
Alan <Mac@Alans-MacBook-Pro.local>
2015-11-30 18:21:51 -0500

    gallery funcitoning, awaiting final menu content & css

--- js/main.js
+++ js/main.js
@@ -4,5 +4,55 @@ $(document).ready(function() {
 		$(this).toggleClass("navActive"); // "this" refers to parent, so this case, navBtn
 		
 
-	}); 
-});
+	}); //end click function
+
+var clickCount = 0;
+	var sliderTotal = $("ul.gallery-slider li").size(); 	//alert(sliderTotal)
+
+	function checkArrows(target) {
+		console.log(target);
+		if (target <=0) {
+			$("#slider-previous").hide();
+			} else if (target > 0 && target < sliderTotal-1) {
+				$("#slider-previous").show();
+				$("#slider-next").show();
+			}
+			else if (target >= sliderTotal-5) {
+				$("#slider-next").hide();
+
+			}
+		}; //checkArrows
+
+	checkArrows(clickCount);	
+
+	
+
+
+	$("#slider-next").click(function() {
+		//console.log("click test, slider next");
+		if (clickCount >= sliderTotal-1) {
+
+		} else {
+			$("ul.gallery-slider").animate({
+				"margin-left" : "-=500px"
+			}); //animate method end
+			clickCount++;
+			checkArrows(clickCount);
+		}; // end if
+
+	}); //click method end
+
+	$("#slider-previous").click(function() {
+		//console.log("click test, slider prev");
+		if (clickCount <=0) {
+
+		} else {
+		$("ul.gallery-slider").animate({
+			"margin-left" : "+=500px"
+		}); //animate end
+		clickCount--;
+		checkArrows(clickCount);
+		}; //end if
+	}); //click method end
+
+}); //end ready
